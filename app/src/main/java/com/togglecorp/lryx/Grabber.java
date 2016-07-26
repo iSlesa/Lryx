a51aec6f0ac7626d11851ccb77c2f6fea41084d5
slesa <artycrab_slesh@yahoo.com>
2016-07-26 19:38:08 +0545

    Add exception handling

--- app/src/main/java/com/togglecorp/lryx/Grabber.java
+++ app/src/main/java/com/togglecorp/lryx/Grabber.java
@@ -15,12 +15,14 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.InvalidParameterException;
 
 public class Grabber extends AsyncTask<Void, Void, Void>{
 
     private String query;
     private String lyrix;
     private String track;
+    private String error;
     private Callback callback;
 
     public Grabber(String q, Callback cb){
@@ -30,34 +32,37 @@ public class Grabber extends AsyncTask<Void, Void, Void>{
 
     protected Void doInBackground(Void... params){
         // public static String grab_lyrics(String arg) throws IOException{
-        String arg = query;
-        arg = arg.replaceAll(" ","+");
-        Log.d("query", arg);
-        String Url = "http://google.com/search?hl=en&q="+arg+"+lyrics&btnI=I\'m+Feeling+Lucky";
-        Log.d("url", Url);
-        URL url = null;
         try {
-            url = new URL(Url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        HttpURLConnection ucon = null;
-        try {
-            ucon = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d("Here","vayena ni ta");
-        }
-        ucon.setInstanceFollowRedirects(false);
-        String redirectedUrl = ucon.getHeaderField("Location");
+            error = "";
+            String arg = query;
+            arg = arg.replaceAll(" ","+");
+            Log.d("query", arg);
+            String Url = "http://google.com/search?hl=en&q="+arg+"+lyrics&btnI=I\'m+Feeling+Lucky";
+//        Log.d("url", Url);
+//        URL url = null;
+//        try {
+//            url = new URL(Url);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        HttpURLConnection ucon = null;
+//        try {
+//            ucon = (HttpURLConnection) url.openConnection();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            Log.d("Here","vayena ni ta");
+//        }
+//        ucon.setInstanceFollowRedirects(false);
+//        String redirectedUrl = ucon.getHeaderField("Location");
 
-        Document page = null;
-        try {
-            page = Jsoup.connect(redirectedUrl).userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6").timeout(30000)
+            Document page = null;
+            page = Jsoup.connect(Url).userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6").timeout(30000)
                     .referrer("http://www.google.com")
                     .get();
             Log.d("Title: ", page.title());
             track = page.title();
+            int a = track.indexOf("-");
+            track = track.substring(a+2,track.length());
             page.outputSettings(new Document.OutputSettings().prettyPrint(false));//makes html() preserve linebreaks and spacing
             page.select("br").append("\\n");
             Elements lyrics = page.select("b + br + br + div"); //Search for lyrics <div> tag, that after <b> and 2 <br> tags
@@ -65,17 +70,26 @@ public class Grabber extends AsyncTask<Void, Void, Void>{
             lyrix = Jsoup.clean(s, "", Whitelist.none(), new Document.OutputSettings().prettyPrint(false));
             lyrix = lyrix.replace("\n\n", "\n");
             lyrix = lyrix.substring(4); //Remove first enters
-
-            Log.d("lyrics", lyrix );
-        } catch (IOException e) {
+            //Log.d("lyrics", lyrix );
+        } catch (Exception e) {
             e.printStackTrace();
-
+            Log.d("Error msg", "Exception aaaaayoooo!");
+            error = "Some error which I have no idea about. Peace.";
         }
+
 
         return null;
     }
 
     protected void onPostExecute(Void param) {
-        callback.gotSomething(lyrix, track );
+        if (error.length()==0) {
+            if (lyrix.length() != 0)
+                callback.gotSomething(lyrix, track);
+            else
+                callback.gotSomething("Couldn't find the lryx.", "Sorry :(");
+        }
+        else{
+            callback.gotSomething(error,"Sorry :(");
+        }
     }
 }
